d742b0d57731fa9597a24dae72a2933b8dd9fcde
meylis <meylismatiyev@gmail.com>
2021-07-26 00:06:30 +0200

    next.config.js updated

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   assetPrefix: isProd ? 'https://meylism.github.io/personal-website/' : '',
-  basePath: isProd ? 'https://meylism.github.io/personal-website/' : '',
+  basePath: isProd ? '/personal-website' : '',
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
