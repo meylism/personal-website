25ed8038518c49aa3a0281989fb32566707dd96c
meylis <meylismatiyev@gmail.com>
2021-07-25 16:35:08 +0200

    basePath added

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   assetPrefix: isProd ? 'https://meylism.github.io/personal-website/' : '',
+  basePath: isProd ? 'https://meylism.github.io/personal-website/' : '',
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
