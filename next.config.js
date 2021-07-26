68b9027b2914f63809d4c7b0a7d371066442cb26
meylis <meylismatiyev@gmail.com>
2021-07-26 11:15:56 +0200

    basePath added

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
