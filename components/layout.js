d110fe72de8c553279724a6ed48be78eb1359355
meylis <meylismatiyev@gmail.com>
2021-07-26 19:14:37 +0200

    Google Analytics added

--- components/layout.js
+++ components/layout.js
@@ -12,6 +12,16 @@ export default function Layout({children, home, post}) {
                 name="description"
                 content="Personal blog."
             />
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-42W67DYZER"></script>
+            <script dangerouslySetInnerHTML={{__html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                    dataLayer.push(arguments) 
+                }
+                gtag("js", new Date());
+                gtag("config", "G-42W67DYZER");
+            `}}>
+            </script>
         </Head>
             <header className={styles.header}>
                 <nav>
