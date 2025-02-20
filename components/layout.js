ee0273aa1beb5b7b6e6d5117da91aaa92268fb4c
Meylis Matiyev <meylis.matiyev@genesys.com>
2025-02-20 21:19:40 +0100

    remove keybase

--- components/layout.js
+++ components/layout.js
@@ -36,11 +36,6 @@ export default function Layout({children, home, post}) {
                                 <img src="/static/icons/github.svg" alt="GitHub" className={styles.linkContainer__logo}/>
                             </a>
                         </li>
-                        <li>
-                            <a href="https://keybase.io/meylism" target="_black" title="Keybase">
-                                <img src="/static/icons/keybase.svg" alt="Keybase" className={styles.linkContainer__logo}/>
-                            </a>
-                        </li>
                     </ul>
                 </div>
             </header>
